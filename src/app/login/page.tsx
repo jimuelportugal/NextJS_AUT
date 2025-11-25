810655432f3b4b26eb6504c5cd67755042232b59
IRyS <anonymous0358798@gmail.com>
2025-11-25 09:01:43 +0800

    login page fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,17 @@
 'use client';
-import { useRouter } from "next/navigaton";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { saveToken } from "@/lib/auth";
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Card, CardContent } from '@/components/ui/card';
-
 import { API_BASE } from "@/lib/config";
 import { FormEvent } from "react";
 import { error } from "console";
 
 export default function LoginPage() {
 
-    const router = userRouter();
+    const router = useRouter();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
@@ -32,22 +31,22 @@ export default function LoginPage() {
         return;
     }
     saveToken(data.accessToken);
-    router.push('/dashboard');
+        router.push('/dashboard');
 }
 
 return (
     <div className="flex itemsa-center justify-center h-screen">
-        <card className="w-full max-w-sm p-6">
+        <Card className="w-full max-w-sm p-6">
             <cardContent>
                 <h1 className="text-xl font-bold md-4">LOGIN</h1>
                 <form onSubmit={handleLogin} className="space-y-4">
-                    <input placeholder="Usernamr" value={username} onChange={(e) => setUsername(e.target.value)} />
-                    <input type="passsword" placeholder="Passsword" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <Input placeholder="Usernamr" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <Input type="passsword" placeholder="Passsword" value={password} onChange={(e) => setPassword(e.target.value)}/>
                     {error && <p className="text-red-500 text-sm">{error}</p>}
                 </form>
-                <button variant="link" className="mt-2 w-full" onClick={() => router.push('/register')}>Create an Account</button>
+                <Button variant="link" className="mt-2 w-full" onClick={() => router.push('/register')}>Create an Account</Button>
             </cardContent>
-        </card>
+        </Card>
     </div>
 );
 }

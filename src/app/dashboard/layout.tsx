90997a73dc9608f7d9aabe2137ce3e4d69575644
IRyS <anonymous0358798@gmail.com>
2025-12-01 08:54:21 +0800

    YES

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -10,7 +10,7 @@ export default function DashboardLayout({ children }: {
     const token = getToken();
 
     if (!token) {
-        router.push('/login');
+        router.push('/');
         return null;
     }
 

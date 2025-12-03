b0310dc941e60bdde3c177313d1ec6f8bbce7584
IRyS <anonymous0358798@gmail.com>
2025-12-03 08:46:34 +0800

    Navbar in login/register removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -124,11 +124,11 @@ function RegisterForm() {
 export default function Home() {
   return (
     <div className="min-h-screen font-sans bg-[#2d3250] dark:bg-black">
-      <header className="sticky top-0 z-50 w-full">
+      {/* <header className="sticky top-0 z-50 w-full">
         <div className="max-w-7xl px-4 sm:px-6 lg:px-8">
           <NavBar />
         </div>
-      </header>
+      </header> */}
       <div className="pl-30 pt-35 max-w-5xl mx-auto text-center flex flex-row h-auto">
         <Card className="bg-[#424769] border-none rounded-l-lg rounded-r-none text-white shadow-2xl border border-gray-700 backdrop-blur-sm duration-300">
           <CardContent>

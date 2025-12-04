831bbdd9b6fe7d96e6755bbb7131036e816b6bc8
IRyS <anonymous0358798@gmail.com>
2025-12-04 11:31:19 +0800

    use client fixed

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,5 @@
-import * as React from "react";
+
+'use client';import * as React from "react";
 import { useState, useEffect } from "react";
 import { getToken, logoutUser } from "@/lib/auth";
 import type { CustomJwtPayload } from "@/lib/auth"; // FIX: Added import type
